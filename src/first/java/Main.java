7b12f23150d0d27cb4e7dc9cab6c88cc1cf5df4a
Beatrice <beatrice.bucur@gmail.com>
2018-12-15 23:21:47 +0200

    first project commit

---
+++ src/first/java/Main.java
@@ -0,0 +1,13 @@
+package first.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("My Christmas TO DO LIST");
+        System.out.println("1 - Buy presents");
+        System.out.println("2 - Wrap gifts");
+        System.out.println("3 - Give presents");
+
+    }
+}
