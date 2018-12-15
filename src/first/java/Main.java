2e2187d4f8cb69bd2218deeb4c0ccac6a5a98fb7
Beatrice <beatrice.bucur@gmail.com>
2018-12-15 23:36:00 +0200

    Updated no.1 task on my to do list

--- src/first/java/Main.java
+++ src/first/java/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("My Christmas TO DO LIST");
-        System.out.println("1 - Buy presents");
+        System.out.println("1 - BE present");
         System.out.println("2 - Wrap gifts");
         System.out.println("3 - Give presents");
 
