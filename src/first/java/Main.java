1266a100b66f3e5b9d97275243af09db4f1ab519
Beatrice <beatrice.bucur@gmail.com>
2018-12-15 23:36:33 +0200

    Updated no.2 task on my to do list

--- src/first/java/Main.java
+++ src/first/java/Main.java
@@ -6,7 +6,7 @@ public class Main {
 	// write your code here
         System.out.println("My Christmas TO DO LIST");
         System.out.println("1 - BE present");
-        System.out.println("2 - Wrap gifts");
+        System.out.println("2 - Wrap SOMEONE in a HUG");
         System.out.println("3 - Give presents");
 
     }
